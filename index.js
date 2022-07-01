746a05bfe054906e4bb221cfc31c65c0bac9c08a
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-07-01 18:03:06 +0530

    feat: implemented restart game

--- index.js
+++ index.js
@@ -4,19 +4,98 @@ for(let i = 0; i < 9; i++){
     boxes[i].addEventListener("click", inputBoxClick)
 }
 
-function inputBoxClick() {}
+const newGameBtn = document.querySelector(".newgame-btn");
+
+
+function inputBoxClick(e){
+    gameBoard.addNewState(parseInt(e.target.getAttribute("data-index")));
+}
 
 const gameBoard = (() => {
-    // let gameBoardStates = [["X","O","X","X","O","","","",""]]
-    let gameBoardStates = [["","","","","","","","",""]]
-    let tool = "X";
+    let newGameState = ["","","","","","","","",""];
+    let gameBoardStates = [[...newGameState]];
+    let tool = "O";
+    let indexOfX = [];
+    let indexOfO = [];
+    const winnerDisplayOutput = document.querySelector(".result-div");
+    const newGameDiv = document.querySelector(".newgame-div");
+    const newGameBgDiv = document.querySelector(".newgame-bg-div");
+    const gameBoxDiv = document.querySelector(".gamebox-div");
 
     const setTool = (param) => {tool = param};
     
     const getPreviousState = () => {
         return gameBoardStates[gameBoardStates.length - 1];
     }
+    
+    const askNewGame = () => {
+        console.log(newGameDiv);
 
+        newGameDiv.style.display = "flex";
+        newGameBgDiv.style.display = "flex";
+        gameBoxDiv.style.filter = "blur(8px)"
+    }
+
+    const resetGame = () => {
+        gameBoardStates = [[...newGameState]];
+        tool = "O";
+        indexOfX = [];
+        indexOfO = [];
+        displayController();
+        winnerDisplayOutput.innerText = "";
+        newGameDiv.style.display = "none";
+        newGameBgDiv.style.display = "none";
+        gameBoxDiv.style.filter = "none"
+
+    }
+
+    const checkWinner = () => {
+        const winPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+        
+        let counterX = 0;
+        let counterO = 0;
+        for(let i = 0; i < winPositions.length; i++){
+            for(let j = 0; j < 3; j++){
+                if(indexOfX.includes(winPositions[i][j])){
+                    counterX++;
+                }
+                else if(indexOfO.includes(winPositions[i][j])){
+                    counterO++;
+                }
+            }
+
+            if(counterX === 3){
+                askNewGame();
+                return "X";
+            }
+            else if(counterO === 3){
+                askNewGame();
+                return "O";
+            }
+            counterX = 0;
+            counterO = 0;
+        }
+        return false;
+    }
+
+    const addToXandO = (tool, index) => {
+        if(tool === "X"){
+            indexOfX.push(index);
+        }
+        else if(tool === "O"){
+            indexOfO.push(index);
+        }
+        
+        if(checkWinner()){
+            displayWinner(checkWinner());
+        };
+    }
+    
+
+    const displayWinner = (tool) => {
+        winnerDisplayOutput.innerText = tool + " is the winner.";
+    }
+    
     const toggleTool = () => {
         tool = (tool === "X") ? "O" : "X";
     }
@@ -25,6 +104,7 @@ const gameBoard = (() => {
         if(getPreviousState()[index] === ""){
             gameBoardStates.push(getPreviousState())
             getPreviousState()[index] = tool;
+            addToXandO(tool, index);
             toggleTool();
             displayController();
         }
@@ -35,17 +115,12 @@ const gameBoard = (() => {
             boxes[i].textContent = getPreviousState()[i]
         }
     }
-    return {setTool, displayController, addNewState};
+    return {resetGame, setTool, displayController, addNewState};
 })();
 
+const startNewGame = () => {
+    gameBoard.resetGame();
+}
+newGameBtn.addEventListener("click", startNewGame)
+
 gameBoard.displayController();
-
-gameBoard.addNewState(0)
-gameBoard.addNewState(1)
-gameBoard.addNewState(8)
-gameBoard.addNewState(7)
-
-gameBoard.addNewState(7)
-gameBoard.addNewState(7)
-gameBoard.addNewState(7)
-gameBoard.addNewState(7)
